4720d2bd19f27e5e98452f0b5184a9c8d08b7e4b
pieroit <piero.savastano@gmail.com>
2025-11-24 23:25:16 +0100

    update statics

--- endpoints/crud.py
+++ endpoints/crud.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 
-from cat import hook
+from cat import hook, endpoint
 from cat.types import Message
 from cat.auth import AuthResource
 from cat.routes import create_crud
@@ -17,11 +17,11 @@ class ChatSelect(CRUDSelect):
     messages: List[Message]
     context: Dict
 
-@hook
-async def after_mad_hatter_refresh(cat):
+@endpoint.router
+def chats_crud():
     router = create_crud(
         db_model=ChatDB,
-        prefix="/chats",
+        prefix="/api/v2/chats",
         tag="Chats",
         auth_resource=AuthResource.CHAT,
         restrict_by_user_id=True,
@@ -30,8 +30,7 @@ async def after_mad_hatter_refresh(cat):
         create_schema=ChatCreateUpdate,
         update_schema=ChatCreateUpdate
     )
+    return router
 
-    cat.fastapi_app.include_router(router)
 
-# TODOV2: endpoints are not removed on plugin deactivation
 # TODOV2: Piccolo offers `create_pydantic_model`
--- endpoints/frontend.py
+++ endpoints/frontend.py
@@ -1,25 +1,34 @@
 import os
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 
 from cat import endpoint
 
-@endpoint.get("/{path:path}", prefix="/ui", include_in_schema=False)
-async def frontend(path: str, req: Request):
+
+@endpoint.get("/", include_in_schema=False)
+async def frontend_index(req: Request) -> HTMLResponse:
 
     ccat = req.app.state.ccat
 
-    ui_static_path = os.path.abspath(
-        os.path.join(ccat.plugin.path, "web")
+    index_path = os.path.abspath(
+        os.path.join(ccat.plugin.path, "dist/index.html")
+    )
+    return FileResponse(path=index_path)
+
+
+@endpoint.get("/assets/{path:path}", include_in_schema=False)
+async def frontend_assets(path: str, req: Request) -> HTMLResponse:
+
+    ccat = req.app.state.ccat
+
+    assets_path = os.path.abspath(
+        os.path.join(ccat.plugin.path, "dist/assets")
     )
 
-    file_path = os.path.abspath(os.path.join(ui_static_path, path))
+    file_path = os.path.abspath(os.path.join(assets_path, path))
 
-    if not file_path.startswith(ui_static_path):
+    if not file_path.startswith(assets_path):
         raise HTTPException(status_code=403, detail="Access forbidden")
-
-    if file_path.endswith("/web"):
-        file_path += "/index.html"
 
     if not os.path.isfile(file_path):
         raise HTTPException(status_code=404, detail="File not found")
